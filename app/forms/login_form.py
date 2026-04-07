cb12cbcce31e58f2c314fcdfc26b1b3f7e7d2a2a
Artem Savely <art.sav07@yandex.ru>
2026-04-07 20:10:36 +0300

    authentification and registration development

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.fields.simple import StringField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
+    username = StringField("Имя пользователя", validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
--- app/web/pages.py
+++ app/web/pages.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, redirect, render_template
 from flask_login import login_user
-
-
-from ..forms.login_form import LoginForm
+from app.forms import LoginForm, RegisterForm
+from app.services import UserService
 
 
 blueprint = Blueprint('web', __name__)
@@ -10,14 +9,37 @@ blueprint = Blueprint('web', __name__)
 
 @blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = authenticate(email=form.email.data, password=form.password.data)
-    #     if user:
-    #         login_user(user, remember=form.remember_me.data)
-    #         return redirect("/")
-    #     return render_template('login.html',
-    #                            message="Неправильный логин или пароль",
-    #                            form=form)
-    # return render_template('login.html', title='Авторизация', form=form)
+    form = LoginForm()
+    user_service = UserService()
+    if form.validate_on_submit():
+        user = user_service.authenticate(email=form.email.data, password=form.password.data)
+        if user:
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    user_service = UserService()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html',
+                                   title="Регистрация",
+                                   form=form,
+                                   message="Пароли не совпадают")
+        try:
+            user = user_service.create_user(email=form.email.data,
+                                        username=form.username.data,
+                                        password=form.password.data)
+        except ValueError as e:
+            return render_template('register.html',
+                                   title="Регистрация",
+                                   form=form,
+                                   message=str(e)
+                                   )
+        return redirect("/")
+    return render_template('register.html', title='Регистрация', form=form)
